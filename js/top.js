9ab893c7213932be534e7134ddd3af95e0277cf7
soos <shinsh1017@gmail.com>
2021-01-21 10:23:44 +0900

    _

--- js/top.js
+++ js/top.js
@@ -15,331 +15,262 @@ var red = 'rgb(128, 0, 0,1)';
 var pinkred = 'rgb(165,  42,  42,1)';
 var shinered = 'rgb(220, 20, 60,1)';
 
-//========================function initAudioPlayer =============================
-function initAudioPlayer(){
+let top_id, top_sig, _audio, songDir, stopbtn, pausebtn, mutebtn, shufflebtn, prebtn, nextbtn,
+    speedlist, selectlist, playslider, volslider, seeking, pos, play_index, play_title,
+    plustime, minustime, extlist, ext, title, type, agent, time_set;
 
-  var audio, songDir, stopbtn, pausebtn, mutebtn, shufflebtn, prebtn, nextbtn,
-      speedlist, selectlist, playslider, volslider, seeking, pos, play_index, play_title,
-      plustime, minustime, extlist, ext, title, type, agent;
+//var playlist = [//"Jerusalema_Master KG.wav",
+            //"You Get What You Give (Original)_New Radicals.mp3",
+            //"Centro di gravita permanente_Franco Battiato.wav",
+  //          "kor_ko.wav"];
+
+top_id = "i_top";
+songDir = "./song/";
+extlist = [".wav", ".mp3", ".ogg"];
+
+//============================================================================
+function set_audio(newlist){
 
   songDir = "./song/";
 
-  playlist = ["Jerusalema_Master KG.wav",
-              "You Get What You Give (Original)_New Radicals.mp3",
-              "Centro di gravita permanente_Franco Battiato.wav",
-              "kor_ko.wav"];
-
+  playlist = newlist;
   play_index = 0;
 
-  extlist = [".wav", ".mp3", ".ogg"];
-  split_title();
-  //check_agent();
+  console.log("playlist:",playlist);
 
-  //========================get id==============================================
-  stopbtn = document.getElementById("stop");
-  pausebtn = document.getElementById("pause");
+  _audio = $('#top').contents().find('#audio');//audio = new Audio();//  audio.append(`<audio id='audio'></audio>`);
 
-  prebtn = document.getElementById("pre");
-  nextbtn = document.getElementById("next");
+  _audio.attr('src',`${songDir}${playlist[play_index]}`);//audio.src = songDir + playlist[play_index];
+  _audio.attr('load',`${songDir}${playlist[play_index]}`);
 
-  mutebtn = document.getElementById("vol");
-  volslider = document.getElementById("vol_bar");
+  if(playlist.length === 1){
+    _audio.attr('loop',true);
+  }else{
+    _audio.attr('loop',false);
+  }
 
-  shufflebtn = document.getElementById("random");
-  speedlist = document.getElementById("speed");
-  selectlist = document.getElementById("select_playlist");
+  //split_title();
+  //play_title.html(`Track ${play_index+1} - ${title}`);
+  delete_selectlist();
+  make_selectlist();
+  act_play();
 
-  playslider = document.getElementById("play_bar");
-  plustime = document.getElementById("play_plus");
-  minustime = document.getElementById("play_minus");
+}
 
-  play_title = document.getElementById("playtitle");
+//========================function initAudioPlayer =============================
 
+//========================function============================================
+//========================play btn
+function act_play(){
 
-  //============================================================================
-  audio = new Audio();
-  audio.src = songDir + playlist[0];
-  audio.loop = false;
-  //audio.play();
-  //play_title.innerHTML = "Track" + (play_index+1) + " - " + playlist[0];
+  let stopbtn = $('#top').contents().find('#stop');
+  let pausebtn = $('#top').contents().find('#pause');
+  let play_title = $('#top').contents().find('#playtitle');
+
+  _audio[play_index].play();
+
+  stopbtn.attr('value', ">");
+  stopbtn.css('color',white);
+  play_title.html(`Track ${play_index+1} - ${playlist[0]}`);
+
+  pausebtn.attr('value', "|||");
+  pausebtn.css("color",shinegreen);
+
+}
+
+//========================check playing and then pause
+function check_playingPause(){
+  let stopbtn = $('#top').contents().find('#stop');
+  let pausebtn = $('#top').contents().find('#pause');
+
+  if (pausebtn.val() === ">>>"){
+    _audio[play_index].pause();
+    stopbtn.attr('value',"=");
+    stopbtn.css('color',shinegreen);
+
+    pausebtn.attr('value',"|||");
+    pausebtn.css('color',shinegreen);
+  }
+}
+
+//========================selectlist
+//========================기존 seleclist삭제
+function delete_selectlist(){
+//  top_sig = "delete_selectlist";
+//  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+  console.log("i_top_____delete_selectlist");
+  selectlist = $('#top').contents().find('#select_playlist');
+  selectlist.empty();//https://blog.naver.com/keiaz/220971558830 ,https://blog.naver.com/ttotto4417/222212079744
+
+}
+//========================songDir -> seleclist추가
+function make_selectlist(){
+//  top_sig = "make_selectlist";
+//  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+  selectlist = $('#top').contents().find('#select_playlist');
+
+  for (var i = 0; i < playlist.length; i++){
+
+    var title = playlist[i].split(".");
+    title = title[0];
+    console.log('playlist[i]:',playlist[i])
+
+    selectlist.append(`<option value = ${i} value2=${playlist[i]} id=${playlist[i]}>${title}</option>`);
+  }
+}
+
+//========================check browser
+/*//파일 종류 선택 재생 가능할 때 사용
+function check_agent(){
+  agent = navigator.userAgent.toLowerCase();
+  if(agent.indexOf('firefox') != -1 || agent.indexOf('opera') != -1){
+    ext = ext[2];
+  }
+}
+*/
+
+//========================check file type
+//파일 타입 알아내서 재생할 때 사용 //(상황따라)확장자 error 처리하는 법 추가하기
+function split_title(){
+
+  for(var i = 0; i < extlist.length; i++){
+    type = playlist[play_index].split(extlist[i]);
+    if (type.length != 1) {
+      ext =extlist[i];
+      title = type[0];
+      break;
+    }
+  }
+}
+
+//==============================================================================
+//===============================i_top==========================================
+//========================get_items=============================================
+
+function  get_items(){
+  //document.getElementById("stop");
+  _audio = $('#top').contents().find('#audio');
+
+  stopbtn = $('#top').contents().find('#stop');
+  pausebtn = $('#top').contents().find('#pause');
+
+  prebtn = $('#top').contents().find('#pre');
+  nextbtn = $('#top').contents().find('#next');
+
+  mutebtn = $('#top').contents().find('#vol');
+  voltext = $('#top').contents().find('#vol_text');
+  volslider = $('#top').contents().find('#vol_bar');
+
+  shufflebtn = $('#top').contents().find('#random');
+  speedlist = $('#top').contents().find('#speed');
+  selectlist = $('#top').contents().find('#select_playlist');
+
+  playslider = $('#top').contents().find('#play_bar');
+  plustime = $('#top').contents().find('#play_plus');
+  minustime = $('#top').contents().find('#play_minus');
+
+  play_title = $('#top').contents().find('#playtitle');
 
   //========================add event===========================================
-  stopbtn.addEventListener("click", act_stop);
-  pausebtn.addEventListener("click", act_pause);
+/*
+  stopbtn.attr("onclick", "act_stop()");//addEventListener("click", act_stop);
+  pausebtn.attr("onclick", "act_pause()");
 
-  prebtn.addEventListener("click", act_pre);
-  nextbtn.addEventListener("click", act_next);
+  prebtn.attr("onclick", "act_pre()");//.addEventListener("click", act_pre);
+  nextbtn.attr("onclick", "act_next()");//.addEventListener("click", act_next);
 
-  mutebtn.addEventListener("click", act_mute);
-  volslider.addEventListener("mousemove", set_volume);
-  shufflebtn.addEventListener("click",act_shuffle);
+  mutebtn.attr("onclick", "act_mute()");//.addEventListener("click", act_mute);
+  volslider.attr("mousemove", "set_volume()");//.addEventListener("mousemove", set_volume);
+  shufflebtn.attr("onclick", "act_shuffle()");//.addEventListener("click",act_shuffle);
 
-  speedlist.addEventListener("change", change_speed);
-  selectlist.addEventListener("change", change_selectlist);
+  speedlist.attr("change", "set_speed()");//.addEventListener("change", set_speed);
+  selectlist.attr("change", "change_selectlist()");//.addEventListener("change", change_selectlist);
 
-  playslider.addEventListener("mousedown", function(event){seeking=true; seek_pos(event);});
-  playslider.addEventListener("mousemove", function(event){seek_pos(event)});
-  playslider.addEventListener("mouseup", function(event){seeking=false;});
+  playslider.attr("mousedown", "function(event){seeking=true; seek_pos(event);}");//.addEventListener("mousedown", function(event){seeking=true; seek_pos(event);});
+  playslider.attr("mousemove", "function(event){seek_pos(event)}");//.addEventListener("mousemove", function(event){seek_pos(event)});
+  playslider.attr("mouseup", "function(event){seeking=false;}");//.addEventListener("mouseup", function(event){seeking=false;});
 
-  audio.addEventListener("timeupdate", function(){update_playTimePos();});
-  audio.addEventListener("ended", function(){change_playlist();});
+  _audio.addEventListener("timeupdate", function(){update_playTimePos();});
+  _audio.addEventListener("ended", function(){change_playlist();});
+*/
+}
 
-  make_selectlist();
+//========================stop btn
+function act_stop(){
 
-  //========================function============================================
-  //========================play btn
-  function act_stop(){
-    if(stopbtn.value === "="){
-      audio.play();
-      stopbtn.value = ">";
-      $('#stop').css("color",white);
-      play_title.innerHTML = "Track" + (play_index+1) + " - " + playlist[0];
+  top_sig = "act_stop";
+  console.log("top: ",top_id,top_sig);
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
 
-      pausebtn.value = "|||";
-      $('#pause').css("color",shinegreen);
-    }else{
-      audio.pause();
-      stopbtn.value = "=";
-      $('#stop').css("color",shinegreen);
+}
+//========================pause btn
+function act_pause(){
 
-      pausebtn.value = "|||";
-      $('#pause').css("color",shinegreen);
-    }
+  top_sig = "act_pause";
+  console.log("top: ",top_id,top_sig);
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+
+}
+//========================prebtn
+function act_pre(){
+  top_sig = "act_pre";
+  //console.log('playlist:',playlist, play_index);
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+}
+
+//========================nextbtn
+function act_next(){
+  top_sig = "act_next";
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+
+}
+//========================volume
+function act_mute(){
+  top_sig = "act_mute";
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+}
+function set_volume(){
+  top_sig = "set_volume"
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+}
+
+//========================speed
+function set_speed(){
+  top_sig = "set_speed";
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+}
+
+//========================slider pos
+function seek_pos(event){
+  if (seeking){
+    top_sig = "seek_pos";
+    window.parent.postMessage([top_id,top_sig,event.clientX], 'http://localhost:3000/iframe.html');
   }
-  //========================pause btn
-  function act_pause(){
-    if(stopbtn.value === ">" && pausebtn.value === ">>>"){
-      audio.play();
-      pausebtn.value = "|||";
-      $('#pause').css("color",shinegreen);
-    }else if(stopbtn.value === ">"){
-      audio.pause();
-      pausebtn.value = ">>>";
-      $('#pause').css("color",white);
+}
+//========================play time
+function update_playTimePos(){
+  top_sig = "update_playTimePos";
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+}
+//========================play next song & change play title
+function change_playlist(){
+  top_sig = "change_playlist";
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
+}
 
-    }
-  }
+//========================play selectlist & act btn & change playtitle
+//리스트로 넘길 수 있는 postMessage의 data maxim이 3개 -> id 제외하고 넘김
+//추후 수정하기 -> id 포함해서 넘기는 방식으로
+function change_selectlist(event){
+  top_sig = "change_selectlist";
+  let select_index = $("#select_playlist option:selected").attr('value');
+  let select_src = $("#select_playlist option:selected").attr('id');
 
-  //========================check playing and then pause
-  function check_playingPause(){
-    if (audio.playing === true){
-      audio.pause();
-      stopbtn.value = "=";
-      $('#stop').css("color",shinegreen);
-
-      pausebtn.value = "|||";
-      $('#pause').css("color",shinegreen);
-    }
-  }
-
-  //========================prebtn
-  function act_pre(){
-
-    check_playingPause();
-
-    if (play_index !== 0){
-      play_index-=1;
-    }
-
-    audio.src = songDir + playlist[play_index];
-
-    play_title.innerHTML = "Track " + (play_index + 1) + " - " + playlist[play_index];
-    stopbtn.value = ">";
-    $('#stop').css("color",white);
-
-    audio.play();
-  }
-
-  //========================nextbtn
-  function act_next(){
-
-   check_playingPause();
-
-   if (play_index !== playlist.length-1){
-     play_index+=1;
-   }
-
-   audio.src = songDir + playlist[play_index];
-
-   play_title.innerHTML = "Track " + (play_index + 1) + " - " + playlist[play_index];
-   stopbtn.value = ">";
-   $('#stop').css("color",white);
-
-   audio.play();
-
-  }
-
-  //========================act shuffle //error
-  function act_shuffle() {
-    var randomlist, randomNum, limit, count;
-
-    check_playingPause();
-
-    randomlist = ['','','',''];
-    limit = playlist.length;
-    count = 0;
-
-    while (count < limit){
-      randomNum = Math.floor(Math.random() * limit);
-      if(randomlist[randomNum] !== playlist[randomNum] && randomlist[randomNum] === ''){
-        randomlist[randomNum] = playlist[count];
-        count+=1;
-      }
-    }
-
-    playlist = randomlist;
-    play_index = 0;
-    audio.src = songDir + playlist[play_index];
-
-    play_title.innerHTML = "Track " + (play_index + 1) + " - " + playlist[play_index];
-    stopbtn.value = ">";
-    $('#stop').css("color",white);
-
-    delete_selectlist();
-    make_selectlist();
-
-    audio.play();
-  }
-
-  //========================volume
-  function act_mute(){
-    if(audio.muted){
-      audio.muted = false;
-      mutebtn.value = "Vol";
-      $('#vol').css("color",shinegreen);
-
-    }else{
-      audio.muted = true;
-      mutebtn.value = "X";
-      $('#vol').css("color",white);
-
-    }
-  }
-  function set_volume(){
-    audio.volume = volslider.value / 100;
-    vol_text.innerHTML = volslider.value;
-    //$('#vol_text').css("color",shinegreen);
-  }
-
-  //========================speed
-  function change_speed(event){
-    audio.playbackRate = event.target.value;
-  }
-
-  //========================play selectlist & act btn & change playtitle
-  function change_selectlist(event){
-   audio.src = songDir + event.target.value;// +extlist[0];
-
-   stopbtn.value = ">";
-   $('#stop').css("color",white);
-   change_playlist();
-
-   audio.play();
- }
-
-  //========================slider pos
-  function seek_pos(event){
-    if(seeking){
-      playslider.value = event.clientX - playslider.offsetLeft;
-      pos = audio.duration * (playslider.value / 100);
-      audio.currentTime = pos;
-    }
-  }
-  //========================play time
-  function update_playTimePos(){
-    var nowtime = audio.currentTime * (100 / audio.duration);
-    var remain = audio.duration - audio.currentTime;
-    playslider.value = nowtime;
-
-    //var totalmin = Math.floor(audio.duration / 60);
-    //var totalsec = Math.floor(audio.duration - totalmin * 60);
-    var nowmin = Math.floor(audio.currentTime / 60);
-    var nowsec = Math.floor(audio.currentTime - nowmin * 60);
-    var remainmin = Math.floor(remain / 60);
-    var remainsec = Math.floor(remain % 60);
-
-    //if(totalsec < 10){totalsec = "0" + totalsec;}
-    //if(totalmin < 10){totalmin = "0" + totalmin;}
-    if(nowsec < 10){nowsec = "0" + nowsec;}
-    if(nowmin < 10){nowmin = "0" + nowmin;}
-    if(remainmin < 10){remainmin = "0" + remainmin;}
-    if(remainsec < 10){remainsec = "0" + remainsec;}
-
-    play_plus.innerHTML = nowmin + ":" + nowsec;
-    play_minus.innerHTML = remainmin + ":" + remainsec;
-  }
-
-  //========================play next song & change play title
-  function change_playlist(){
-    if(play_index === (playlist.length - 1)){
-      play_index = 0;
-    }else{
-      play_index+=1;
-    }
-    split_title();
-
-    play_title.innerHTML = "Track " + (play_index + 1) + " - " + title;
-    audio.src = songDir + playlist[play_index];
-    audio.play();
-  }
-
-  //========================selectlist
-  //========================기존 seleclist삭제
-  //  <option value="kor_ko.wav">kor_ko</option>
-  function delete_selectlist(){
-
-      num = selectlist.children.length;
-      while(num > 0){
-         selectlist.removeChild(selectlist.children[0]);
-         num-=1;
-      }
-
-      console.log('num:',num);//,'nameList:',nameList);
-    }
-  //========================songDir -> seleclist추가
-  //  <option value="kor_ko.wav">kor_ko</option>
-  function make_selectlist(){
-    for (var i = 0; i < playlist.length; i++){
-      var track;
-      title = playlist[i].split(".");
-      title = title[0];
-
-      track = document.createElement("option");
-      track.setAttribute('value',playlist[i]);
-      track.setAttribute('id',title);
-      //track.setAttribute('class','track_list');
-      document.getElementById('select_playlist').appendChild(track);
-
-      track.innerHTML = title;
-
-      selectlist.appendChild(track);
-    }
-  }
-
-
- //========================check browser
- /*//파일 종류 선택 재생 가능할 때 사용
-  function check_agent(){
-    agent = navigator.userAgent.toLowerCase();
-    if(agent.indexOf('firefox') != -1 || agent.indexOf('opera') != -1){
-      ext = ext[2];
-    }
-  }
-  */
-
-  //========================check file type
-  //파일 타입 알아내서 재생할 때 사용 //(상황따라)확장자 error 처리하는 법 추가하기
-  function split_title(){
-    for(var i = 0; i < extlist.length; i++){
-      type = playlist[play_index].split(extlist[i]);
-      if (type.length != 1) {
-        ext =extlist[i];
-        title = type[0];
-        break;
-      }
-    }
-  }
-
-
-
+  console.log(select_index,select_src);
+  window.parent.postMessage([top_sig,select_index,select_src], 'http://localhost:3000/iframe.html');
+}
+//========================act shuffle //error
+function act_shuffle() {
+  top_sig = "act_shuffle";
+  window.parent.postMessage([top_id,top_sig], 'http://localhost:3000/iframe.html');
 }
