133e41b921d600bb676c98d6ba6301d39d32be20
soos <shinsh1017@gmail.com>
2021-02-13 05:40:06 +0900

    POST & edit mylist

--- s.js
+++ s.js
@@ -5,97 +5,123 @@ var path = require('path');
 var qs = require('querystring');
 
 var s_db = require('./js/lib/s_db');
-/*
-let EventEmitter = require('events');
- // 이벤트 객체 생성
-let myEvent = new EventEmitter();
-// event1 등록
-myEvent.addListener('event1', () => {
-  console.log('event 1');
-});
-// event2 등록
-myEvent.on('event2', () => {
-  console.log('event 2')
-});
-// event 호출
-myEvent.emit('event1');
-myEvent.emit('event2');
-});
+var db_load = require('./js/lib/s_db_load.js');
+var db_process = require('./js/lib/s_db_process.js');
+var s_iframe = require('./js/iframe.js');
 
-let s_id = 'i_s';
-let s_sig;
-*/
-var songDir = './song';
-let songlist = fs.readdir(songDir, function(err, files){console.log(files);})
-
-let songData = [];
-var list = ''
-
-function get_songData(){
-  s_db.query('SELECT song,singer,filetype FROM song', function(error, songs){
-  //  console.log(songs)
-    list = ''
-    for(var i =0; i < songs.length; i++){
-      songData[i]=`${songs[i].song}_${songs[i].singer}.${songs[i].filetype}`;
-      list += `<option id="${songData[i]}"></option>`;
-      console.log("songData[i]_________:",songData[i])
-    }
-    console.log("list:________-",list)
-  });
-  return list;
-}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var list = get_songData();
-    console.log(url.parse(_url, true),'queryData:',queryData,'pathname:',pathname); //url info
 
+    var song_list = db_load.get_songData();
+    var mylist_list = db_load.get_mylistData();
+  //  setInterval(db_load.get_songData, 5000);
+  //  setInterval(db_load.get_mylistData, 5000);
+
+    var clearhtml = db_load.get_data;
     path = decodeURIComponent(path);
-    console.log("path:",path);
+    //console.log(url.parse(_url, true),'queryData:',queryData,'pathname:',pathname); //url info
+    //console.log("path:",path);
 
     if(_url === '/'){
       _url = '/iframe.html';
-      //s_song.home(request,response);
-      //songs = s_song.home();
-      //window.parent.postMessage([song_id, song_sig, songData], 'http://localhost:3000/iframe.html');
-    }
-    if(request.url == '/favicon.ico'){
-      response.writeHead(404);
 
+    }
+    if(request.url === '/favicon.ico'){
+      response.writeHead(404);
       response.end();
       return;
-    }if(pathname === '/top.html'){
+    }
+    if(pathname === '/top.html'){
       response.write(`
         <html>
-            <style>
-              .song_db{
-                font-size: 0px;
-                text-align: left;
+          <style>
+            .song_db{
+              font-size: 0px;
+              text-align: left;
 
-                width: 0px;
+              width: 0px;
 
-                white-space:nowrap;
-                overflow:hidden;
-                text-overflow: ellipsis;
+              white-space:nowrap;
+              overflow:hidden;
+              text-overflow: ellipsis;
 
-                padding:0px;
-                margin-top:0px;
-                margin-left:0px;
-                border:0px;
+              padding:0px;
+              margin-top:0px;
+              margin-left:0px;
+              border:0px;
 
-                background-color: rgb(0,0,0,0);
-                color:rgb(0,0,0,0);
+              background-color: rgb(0,0,0,0);
+              color:rgb(0,0,0,0);
+            }
+            .mylist_db{
+              font-size: 0px;
+              text-align: left;
 
-              }
-            </style>
-            <select class="song_db">
-              ${list}
-            </select>
+              width: 0px;
+
+              white-space:nowrap;
+              overflow:hidden;
+              text-overflow: ellipsis;
+
+              padding:0px;
+              margin-top:0px;
+              margin-left:0px;
+              border:0px;
+
+              background-color: rgb(0,0,0,0);
+              color:rgb(0,0,0,0);
+            }
+          </style>
+          <select class="song_db">
+            ${song_list}
+          </select>
+          <select class="mylist_db">
+            ${mylist_list}
+          </select>
         </html>
         `);
-      }
+    }
+    if(request.method === "POST"){
+      console.log("POST_event___________",);
+
+      request.on('data', function(data){
+
+        var data = JSON.parse(data);
+
+      //  console.log("data:", data, data.length, "\n");
+      //  console.log(data.id, data.sig, data.contents);
+
+        //================i_popupMylist==============
+        if(data.id === "i_popupMylist" && data.sig === "create_mylist"){
+          console.log("i_popupMylist_____create_mylist");
+          db_process.insert_mylistData(data.contents);
+        }
+        else if(data.id === "i_popupMylist" && data.sig === "edit_mylist"){
+          console.log("i_popupMylist_____edit_mylist\n");
+        //  console.log(data.contents[0],data.contents[1]);
+          db_process.update_mylistData(data.contents[0],data.contents[1]);
+        }
+        else if(data.id === "i_popupMylist" && data.sig === "delete_mylist"){
+          console.log("i_popupMylist_____delete_mylist");
+        //  console.log("\n",data.contents);
+          db_process.delete_mylistData(data.contents);
+        }
+        else if(data.id === "i_popupMylist" && data.sig === "goto_deleteBtn"){
+          console.log("i_popupMylist_____goto_deleteBtn");
+        //  console.log("\n",data.contents,data.contents[0],data.contents[1],data.contents[1][0],data.contents[1][1]);
+          db_process.delete_mylistsongsData(data.contents);
+        }
+        else if(data.id === "i_popupMylist" && data.sig === "add_songToMylist"){
+          console.log("i_popupMylist_____add_songToMylist");
+        //  console.log("\n",data);
+        //  console.log("\n",data.contents);
+          db_process.insert_mylistsongsData(data.contents);
+        }
+      });
+    }
 //  response.writeHead(200);
   response.end(fs.readFileSync(decodeURIComponent(__dirname + _url)));
 
